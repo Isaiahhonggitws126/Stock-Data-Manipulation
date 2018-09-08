4ac121b3d7edf59f839dbe3de39bdb3667f94b38
Isaiah Hong <34777406+Isaiahhonggitws126@users.noreply.github.com>
2018-09-08 15:19:37 -0400

    Updated main.py
    - Added more comments to explain more of my code
    - Added another function which calculates the moving average for 20 days

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from matplotlib import style
 import pandas as pd
 import pandas_datareader.data as web
 
+
 style.use('ggplot')
 start = dt.datetime(2015, 1, 1)
 end = dt.datetime.now()
@@ -21,21 +22,30 @@ df.to_csv('VOO.csv')
 df = pd.read_csv('VOO.csv', parse_dates=True, index_col=0)
 
 df['100ma'] = df['Adj Close'].rolling(window=100,min_periods=0).mean()
+# calculates the moving average for 100 days
+df['20ma'] = df['Adj Close'].rolling(window=50, min_periods=0).mean()
+# calculates the moving average for 20 days
 
-plot_1 = plt.subplot2grid((6,1), (0,0), rowspan=5, colspan = 1)
-plot_2 = plt.subplot2grid((6,1), (5,0), rowspan=1, colspan=1,sharex=plot_1)
+
+plot_1 = plt.subplot2grid((6,1), (0,0), rowspan= 5, colspan = 1)
+plot_2 = plt.subplot2grid((6,1), (5,0), rowspan= 1, colspan= 1,sharex= plot_1)
+
+
 
 plot_1.plot(df.index, df['Adj Close'])
 plot_1.plot(df.index, df['100ma'])
+# plots the moving average for 100 rolling days
+plot_1.plot(df.index, df['20ma'])
+# plots the moving average for 20 rolling days
+plot_1.legend()
+
+
 # plots the Adj close and 100ma
 
-
 plot_2.plot(df.index, df['Volume'])
+plot_2.legend()
+
 # plots the volume of the fund
 
 
-
 plt.show()
-
-
-
